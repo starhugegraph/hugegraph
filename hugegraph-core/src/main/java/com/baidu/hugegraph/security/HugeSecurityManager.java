02382c7d99d638d05f11181680c912baa77f9c99
guoygang <hero.gariker@gmail.com>
2022-02-08 15:28:53 +0800

    删除废弃依赖

--- hugegraph-core/src/main/java/com/baidu/hugegraph/security/HugeSecurityManager.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/security/HugeSecurityManager.java
@@ -80,34 +80,11 @@ public class HugeSecurityManager extends SecurityManager {
             ImmutableSet.of("asyncRemoveIndexLeft")
     );
 
-    private static final Map<String, Set<String>> BACKEND_SOCKET = ImmutableMap.of(
-            // Fixed #758
-            "com.baidu.hugegraph.backend.store.mysql.MysqlStore",
-            ImmutableSet.of("open", "init", "clear", "opened", "initialized")
-    );
-
-    private static final Map<String, Set<String>> BACKEND_THREAD = ImmutableMap.of(
-            // Fixed #758
-            "com.baidu.hugegraph.backend.store.cassandra.CassandraStore",
-            ImmutableSet.of("open", "opened", "init"),
-            // Fixed https://github.com/hugegraph/hugegraph/pull/892#issuecomment-598545072
-            "com.datastax.driver.core.AbstractSession",
-            ImmutableSet.of("execute")
-    );
-
     private static final Map<String, Set<String>> BACKEND_SNAPSHOT = ImmutableMap.of(
             "com.baidu.hugegraph.backend.store.AbstractBackendStoreProvider",
             ImmutableSet.of("createSnapshot", "resumeSnapshot"),
             "com.baidu.hugegraph.backend.store.raft.RaftBackendStoreProvider",
             ImmutableSet.of("createSnapshot", "resumeSnapshot")
-    );
-
-    private static final Set<String> HBASE_CLASSES = ImmutableSet.of(
-            // Fixed #758
-            "com.baidu.hugegraph.backend.store.hbase.HbaseStore",
-            "com.baidu.hugegraph.backend.store.hbase.HbaseStore$HbaseSchemaStore",
-            "com.baidu.hugegraph.backend.store.hbase.HbaseStore$HbaseGraphStore",
-            "com.baidu.hugegraph.backend.store.hbase.HbaseSessions$RowIterator"
     );
 
     private static final Set<String> RAFT_CLASSES = ImmutableSet.of(
@@ -167,7 +144,6 @@ public class HugeSecurityManager extends SecurityManager {
     public void checkAccess(Thread thread) {
         if (callFromGremlin() && !callFromCaffeine() &&
             !callFromAsyncTasks() && !callFromEventHubNotify() &&
-            !callFromBackendThread() && !callFromBackendHbase() &&
             !callFromRaft() && !callFromSofaRpc()) {
             throw newSecurityException(
                   "Not allowed to access thread via Gremlin");
@@ -179,7 +155,6 @@ public class HugeSecurityManager extends SecurityManager {
     public void checkAccess(ThreadGroup threadGroup) {
         if (callFromGremlin() && !callFromCaffeine() &&
             !callFromAsyncTasks() && !callFromEventHubNotify() &&
-            !callFromBackendThread() && !callFromBackendHbase() &&
             !callFromRaft() && !callFromSofaRpc()) {
             throw newSecurityException(
                   "Not allowed to access thread group via Gremlin");
@@ -207,8 +182,7 @@ public class HugeSecurityManager extends SecurityManager {
 
     @Override
     public void checkRead(FileDescriptor fd) {
-        if (callFromGremlin() && !callFromBackendSocket() &&
-            !callFromRaft() && !callFromSofaRpc()) {
+        if (callFromGremlin() && !callFromRaft() && !callFromSofaRpc()) {
             throw newSecurityException("Not allowed to read fd via Gremlin");
         }
         super.checkRead(fd);
@@ -217,9 +191,8 @@ public class HugeSecurityManager extends SecurityManager {
     @Override
     public void checkRead(String file) {
         if (callFromGremlin() && !callFromCaffeine() &&
-            !readGroovyInCurrentDir(file) && !callFromBackendHbase() &&
-            !callFromSnapshot() && !callFromRaft() &&
-            !callFromSofaRpc()) {
+            !readGroovyInCurrentDir(file) && !callFromSnapshot() &&
+            !callFromRaft() && !callFromSofaRpc()) {
             throw newSecurityException(
                   "Not allowed to read file via Gremlin: %s", file);
         }
@@ -237,8 +210,7 @@ public class HugeSecurityManager extends SecurityManager {
 
     @Override
     public void checkWrite(FileDescriptor fd) {
-        if (callFromGremlin() && !callFromBackendSocket() &&
-            !callFromRaft() && !callFromSofaRpc()) {
+        if (callFromGremlin() && !callFromRaft() && !callFromSofaRpc()) {
             throw newSecurityException("Not allowed to write fd via Gremlin");
         }
         super.checkWrite(fd);
@@ -282,8 +254,7 @@ public class HugeSecurityManager extends SecurityManager {
 
     @Override
     public void checkConnect(String host, int port) {
-        if (callFromGremlin() && !callFromBackendSocket() &&
-            !callFromBackendHbase() && !callFromRaft() && !callFromSofaRpc()) {
+        if (callFromGremlin() &&  !callFromRaft() && !callFromSofaRpc()) {
             throw newSecurityException(
                   "Not allowed to connect socket via Gremlin");
         }
@@ -338,7 +309,7 @@ public class HugeSecurityManager extends SecurityManager {
     @Override
     public void checkPropertyAccess(String key) {
         if (!callFromAcceptClassLoaders() && callFromGremlin() &&
-            !WHITE_SYSTEM_PROPERTYS.contains(key) && !callFromBackendHbase() &&
+            !WHITE_SYSTEM_PROPERTYS.contains(key) &&
             !callFromSnapshot() && !callFromRaft() &&
             !callFromSofaRpc()) {
             throw newSecurityException(
@@ -434,16 +405,6 @@ public class HugeSecurityManager extends SecurityManager {
         return callFromWorkerWithClass(CAFFEINE_CLASSES);
     }
 
-    private static boolean callFromBackendSocket() {
-        // Fixed issue #758
-        return callFromMethods(BACKEND_SOCKET);
-    }
-
-    private static boolean callFromBackendThread() {
-        // Fixed issue #758
-        return callFromMethods(BACKEND_THREAD);
-    }
-
     private static boolean callFromEventHubNotify() {
         // Fixed issue #758
         // notify() will create thread when submit task to executor
@@ -453,11 +414,6 @@ public class HugeSecurityManager extends SecurityManager {
     private static boolean callFromAsyncTasks() {
         // Async tasks will create thread when submitted to executor
         return callFromMethods(ASYNC_TASKS);
-    }
-
-    private static boolean callFromBackendHbase() {
-        // TODO: remove this unsafe entrance
-        return callFromWorkerWithClass(HBASE_CLASSES);
     }
 
     private static boolean callFromSnapshot() {
---
+++ hugegraph-dist/src/assembly/travis/install-hstore.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -ev
+
+TRAVIS_DIR=`dirname $0`
+HSTORE_DOWNLOAD_ADDRESS=""
