eb6741565a9023db0167f39ff4e01720df8cc9b6
Scorpiour <dongchen_101@hotmail.com>
2022-03-22 16:42:14 +0800

    check path status one time

--- hugegraph-core/src/main/java/com/baidu/hugegraph/kafka/producer/SyncConfProducer.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/kafka/producer/SyncConfProducer.java
@@ -51,15 +51,20 @@ public class SyncConfProducer extends ProducerClient<String, String> {
             String value = entry.getValue();
 
             String[] keyParts = key.split(MetaManager.META_PATH_DELIMITER);
-
+            Boolean isGraphPath = false;
+            Boolean isGraphspacePath = false;
             for(int i = keyParts.length - 1; i > 1; i--) {
                 String part = keyParts[i];
                 if (part.equals(MetaManager.META_PATH_TASK)
                     || part.equals(MetaManager.META_PATH_TASK_LOCK)
                     || part.equals(MetaManager.META_PATH_KAFKA)
                     || part.equals(MetaManager.META_PATH_DDS)) {
-                // filter specified keys only appears after index 2
-                return;
+                    // filter specified keys only appears after index 2
+                    return;
+                } else if (part.equals(MetaManager.META_PATH_GRAPH)) {
+                    isGraphPath = true;
+                } else if (part.equals(MetaManager.META_PATH_GRAPHSPACE)) {
+                    isGraphspacePath = true;
                 }
             }
             
@@ -67,7 +72,7 @@ public class SyncConfProducer extends ProducerClient<String, String> {
             String graphSpace = null;
 
             // Check if graph is filtered, If it is true skip
-            if (key.contains(MetaManager.META_PATH_GRAPH)) {
+            if (isGraphPath) {
                 List<String> info = manager.extractGraphFromKey(key);
                 graphSpace = info.get(0);
                 String graph = info.get(1);
@@ -76,7 +81,7 @@ public class SyncConfProducer extends ProducerClient<String, String> {
                 }
             }
 
-            if (key.contains(MetaManager.META_PATH_GRAPHSPACE)) {
+            if (isGraphspacePath) {
                 if (Strings.isNullOrEmpty(graphSpace)) {
                     graphSpace = manager.extractGraphSpaceFromKey(key);
                 }
