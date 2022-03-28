ff62ea402297d0a672e2e60fd44c7cef337c9a1b
zhoney <zhangyi89817@126.com>
2022-03-28 14:09:35 +0800

    remove api gzip compress (#386)
    Change-Id: Ia20dc898b1b7cca99affb2980fc3ee07df616624

--- hugegraph-api/src/main/java/com/baidu/hugegraph/api/graph/EdgeAPI.java
+++ hugegraph-api/src/main/java/com/baidu/hugegraph/api/graph/EdgeAPI.java
@@ -268,7 +268,6 @@ public class EdgeAPI extends BatchAPI {
 
     @GET
     @Timed
-    @Compress
     @Produces(APPLICATION_JSON_WITH_CHARSET)
     @RolesAllowed({"admin", "$graphspace=$graphspace $owner=$graph " +
                             "$action=edge_read"})
