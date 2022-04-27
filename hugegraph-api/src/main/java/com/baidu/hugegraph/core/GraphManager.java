ce6f6d1b629419e9550c6209e6b440e981a33fe0
zhoney <zhangyi89817@126.com>
2022-04-28 02:15:18 +0800

    improve
    Change-Id: Ie28e0a8e5d9f840fde0ff73ed787dcb6b64a2bc3

--- hugegraph-api/src/main/java/com/baidu/hugegraph/core/GraphManager.java
+++ hugegraph-api/src/main/java/com/baidu/hugegraph/core/GraphManager.java
@@ -35,6 +35,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
@@ -1689,13 +1690,13 @@ public final class GraphManager {
                                     HugeConfig config,
                                     TypedOption<?, ?> option) {
         Object incomingValue = config.get(option);
-        for (String graphName : this.graphs.keySet()) {
-            String[] parts = graphName.split(DELIMITER);
-            HugeGraph hugeGraph = this.graph(graphSpace, parts[1]);
-            if (hugeGraph == null) {
+        for (Map.Entry<String, Graph> entry : this.graphs.entrySet()) {
+            String[] parts = entry.getKey().split(DELIMITER);
+            if (!Objects.equals(graphSpace, parts[0]) ||
+                !Objects.equals(incomingValue, parts[1])) {
                 continue;
             }
-            Object existedValue = hugeGraph.option(option);
+            Object existedValue = ((HugeGraph) entry.getValue()).option(option);
             E.checkArgument(!incomingValue.equals(existedValue),
                             "The option '%s' conflict with existed",
                             option.name());
