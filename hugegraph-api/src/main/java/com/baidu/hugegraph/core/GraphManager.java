2168250c3af9de4786416e0be4e4dfecd19da12e
zhoney <zhangyi89817@126.com>
2022-04-27 15:50:51 +0800

    fix graphs list api and graph get api
    Change-Id: I945302f148c148cbd97cb4d0c1831ab7dd21dc40

--- hugegraph-api/src/main/java/com/baidu/hugegraph/core/GraphManager.java
+++ hugegraph-api/src/main/java/com/baidu/hugegraph/core/GraphManager.java
@@ -1283,11 +1283,8 @@ public final class GraphManager {
 
     public Set<String> graphs(String graphSpace) {
         Set<String> graphs = new HashSet<>();
-        for (String key : this.graphs.keySet()) {
-            String[] parts = key.split(DELIMITER);
-            if (parts[0].equals(graphSpace)) {
-                graphs.add(parts[1]);
-            }
+        for (String key : this.metaManager.graphConfigs(graphSpace).keySet()) {
+            graphs.add(key.split(DELIMITER)[1]);
         }
         return graphs;
     }
@@ -1296,7 +1293,29 @@ public final class GraphManager {
         String key = String.join(DELIMITER, graphSpace, name);
         Graph graph = this.graphs.get(key);
         if (graph == null) {
-            return null;
+            Map<String, Map<String, Object>> configs =
+                    this.metaManager.graphConfigs(graphSpace);
+            if (!configs.containsKey(key)) {
+                return null;
+            }
+            Map<String, Object> config = configs.get(key);
+            String creator = String.valueOf(config.get("creator"));
+            Date createTime = parseDate(config.get("create_time"));
+            Date updateTime = parseDate(config.get("update_time"));
+            try {
+                HugeGraph graph1 = this.createGraph(graphSpace, name,
+                                         creator, config, false);
+                graph1.createTime(createTime);
+                graph1.updateTime(updateTime);
+                this.graphs.put(key, graph1);
+                return graph1;
+            } catch (HugeException e) {
+                if (!this.startIgnoreSingleGraphError) {
+                    throw e;
+                }
+                LOG.error(String.format("Failed to load graph '%s' from " +
+                                        "meta server", key), e);
+            }
         } else if (graph instanceof HugeGraph) {
             return (HugeGraph) graph;
         }
