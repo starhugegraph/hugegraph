f9669da8d63860fc4dec67cfa28dcf68b29c2bf4
GioPhantus <48943213+GioPhantus@users.noreply.github.com>
2022-07-08 11:59:18 +0800

    fix: property match support different types && delete some never-used codes

--- hugegraph-core/src/main/java/com/baidu/hugegraph/auth/HugeResource.java
+++ hugegraph-core/src/main/java/com/baidu/hugegraph/auth/HugeResource.java
@@ -172,7 +172,8 @@ public class HugeResource {
                 return false;
             }
             try {
-                if (!TraversalUtil.testProperty(prop, expected)) {
+                // Accessed data type should match target value type
+                if (!expected.toString().equals(prop.value().toString())) {
                     return false;
                 }
             } catch (IllegalArgumentException e) {
